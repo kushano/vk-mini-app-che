1e0c30dd57cc20460ee6cb08d5c3d0270132fb7f
kushano <43448851+kushano@users.noreply.github.com>
2022-05-24 21:37:25 +0500

    cinema done !

--- src/panels/Cinema.js
+++ src/panels/Cinema.js
@@ -1,50 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import '@vkontakte/vkui/dist/vkui.css';
 import { Panel, PanelHeader, PanelHeaderBack, Button, FixedLayout, Text, ContentCard, Avatar, Spacing } from '@vkontakte/vkui';
 
 import cinema_db from './dbs/cinema.json'
-import { useEffect, useState } from 'react/cjs/react.production.min';
 import { number } from 'prop-types';
+import CardComp from './components/CardComp';
 
 // fetch(cinema_db).then(cinema_db => console.log(cinema_db))
 
 function Cinema(props) {
 	// const count = Array(cinema_db)
-	
-	const count = 5
-	let number = 4
+
+	const count = 12
+	// let number = 1
+	const [number, setNumber] = useState(5)
+
 	const getRandomNumber = (e) => {
 		// window.location.reload(false);
 		// e.preventDefault()
-		number = Math.floor(Math.random() * (count+1))
-		console.log(number)
+		setNumber(Math.floor(Math.random() * (count + 1)))
+		// console.log(number)
 	}
-	// const getRandomInteger = (e) => {
-	// 	e.preventDefault();
-	// 	var v = Object.keys(count).map((obj) => {
-	// 		var size = 0
-	// 		for (obj in count) {
-	// 			size++;
-	// 		}
-	// 		return size;
-	// 	})
-	// 	console.log(Math.floor(Math.random() * count))
-	// 	console.log(v)
-	// }
-
+	// getRandomNumber()
+	var cardMap = Object.keys(cinema_db).map((msg) => 
+		// <div key={msg.toString()}>
+			<ContentCard
+				size="l"
+				header={<Avatar size={150} src={cinema_db[number].img} />}
+				subtitle={cinema_db[number].from}
+				maxHeight={200}
+				text={<div>
+					<Text weight='semibold'>{cinema_db[number].name}</Text>
+					<Text>Режиссёр: {cinema_db[number].dir}</Text>
+					<Text color='#ff0000'>КП: {cinema_db[number].kp} IMDb: {cinema_db[number].imdb}</Text>
+					<Spacing />
+					<Text>{cinema_db[number].desc}</Text></div>
+				}
+			>
+			</ContentCard>
+		// </div>
+	)
+				
 	return (
-		<Panel id={props.id}>
-			{getRandomNumber()}
-			<FixedLayout>
-				<PanelHeader
-					left={<PanelHeaderBack onClick={props.go} data-to="home" />}
-				>чё посмотреть?
-				</PanelHeader>
-				<Spacing></Spacing>
-				<Button type="submit" onClick={getRandomNumber} stretched size="l">скажи</Button>
-				<Spacing></Spacing>
-				<ContentCard
+		// <div>
+			<Panel id={props.id}>
+				<Spacing size={120}></Spacing>
+				<FixedLayout filled vertical='top'>
+					<PanelHeader
+						left={<PanelHeaderBack onClick={props.go} data-to="home" />}
+					>чё посмотреть?
+					</PanelHeader>
+					<Spacing></Spacing>
+					<Button type="submit" onClick={getRandomNumber} stretched size="l">скажи</Button>
+					<Spacing></Spacing>
+					{/* <ContentCard
 					size="l"
 					header={<Avatar size={150} src={cinema_db[number].img}/>}	
 					subtitle={cinema_db[number].from}
@@ -57,9 +67,13 @@ function Cinema(props) {
 						<Text>{cinema_db[number].desc}</Text></div>
 					}
 				>	
-				</ContentCard>
-			</FixedLayout>
-		</Panel>
+				</ContentCard> */}
+				</FixedLayout>
+				{/* <Panel> */}
+					{cardMap[0]}
+				{/* </Panel> */}
+			</Panel>
+		// </div>
 	)
 }
 
--- src/panels/components/CardComp.js
+++ src/panels/components/CardComp.js
@@ -5,23 +5,26 @@ import { ContentCard, Text, Spacing, Button, Avatar } from '@vkontakte/vkui';
 
 import cinema_db from '../dbs/cinema.json'
 
-const CardComp = ({ number, ...props }) => {
-
-    <ContentCard
-        size="l"
-        header={<Avatar size={150} src={cinema_db[props.number].img} />}
-        subtitle={cinema_db[props.number].from}
-        maxHeight={200}
-        text={<div>
-            <Text weight='semibold'>{cinema_db[props.number].name}</Text>
-            <Text>Режиссёр: {cinema_db[props.number].dir}</Text>
-            <Text color='#ff0000'>КП: {cinema_db[props.number].kp} IMDb: {cinema_db[props.number].imdb}</Text>
-            <Spacing />
-            <Text>{cinema_db[props.number].desc}</Text></div>
-        }
-    >
-    </ContentCard>
-
+const CardComp = ({ getIf, number, ...props }) => {
+    if (props.getIf == true) {
+        <ContentCard
+            size="l"
+            header={<Avatar size={150} src={cinema_db[props.number].img} />}
+            subtitle={cinema_db[props.number].from}
+            maxHeight={200}
+            text={<div>
+                <Text weight='semibold'>{cinema_db[props.number].name}</Text>
+                <Text>Режиссёр: {cinema_db[props.number].dir}</Text>
+                <Text color='#ff0000'>КП: {cinema_db[props.number].kp} IMDb: {cinema_db[props.number].imdb}</Text>
+                <Spacing />
+                <Text>{cinema_db[props.number].desc}</Text></div>
+            }
+        >
+        </ContentCard>
+    } else {
+        <ContentCard></ContentCard>
+    }
+    <ContentCard></ContentCard>
 }
 
 export default CardComp
